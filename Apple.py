c64a3d61d4dc5ada09dbec5980477cfe7cfc683c
mahsima <mahsimeeb83@gmail.com>
2023-11-30 22:09:26 +0330

    some foolish changes

--- Apple.py
+++ Apple.py
@@ -10,4 +10,4 @@ class Apple_Class:
     def GetApplePosition(self):
 
         Apple_Class.fruit_position = [random.randrange(1, (UI.window_x//10)) * 10,
-				random.randrange(1,((450)//10)) * 10]
+				random.randrange(20,((UI.window_y)//10)) * 10]
